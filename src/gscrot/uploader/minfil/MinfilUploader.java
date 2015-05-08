e41724ab98918734eccdeb14ad63e1f604216cd3
redpois0n <red@cmail.nu>
2015-05-09 01:33:13 +0200

    Minfil uploader working

--- src/gscrot/uploader/minfil/MinfilUploader.java
+++ src/gscrot/uploader/minfil/MinfilUploader.java
@@ -2,6 +2,9 @@ package gscrot.uploader.minfil;
 
 import iconlib.IconUtils;
 
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
 import com.redpois0n.gscrot.Capture;
 import com.redpois0n.gscrot.CaptureUploader;
 import com.redpois0n.gscrot.UploadResponse;
@@ -16,7 +19,25 @@ public class MinfilUploader extends CaptureUploader {
 	public UploadResponse process(Capture capture) throws Exception {
 		String response = Minfil.upload(capture.getBinary(), capture.getFormat().toString());
 		
-		return null;
+		JSONObject jo = (JSONObject) JSONValue.parse(response);
+		
+		if (!jo.get("status").toString().equalsIgnoreCase("true")) {
+			throw new Exception(jo.get("status").toString());
+		}
+		
+		JSONObject file = (JSONObject) jo.get("file");
+		JSONObject url = (JSONObject) file.get("url");
+
+		Object link = url.get("full");
+		
+		if (link != null) {			
+			UploadResponse ur = new UploadResponse(link.toString(), null);
+			ur.setRaw(response);
+			
+			return ur;
+		} else {
+			throw new Exception("Error: " + response);
+		}
 	}
 
 }
